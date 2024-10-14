c6d9068e9e0ee5e9ddc1213acc38f17fb7b9b345
rauchrods <rauchrodrigues@gmail.com>
2024-10-14 23:24:10 +0530

    5- UI (updated profile sec)

--- frontend/src/pages/profile/ProfilePage.jsx
+++ frontend/src/pages/profile/ProfilePage.jsx
@@ -11,6 +11,9 @@ import { MdEdit } from "react-icons/md";
 import PageLayout from "../../components/pageLayout/PageLayout";
 import { POSTS } from "../../utils/db/dummy";
 import Posts from "../../components/common/posts/Posts";
+import "./profilePage.scss";
+import Avatar from "../../ui/avatar/Avatar";
+import Button from "../../ui/button/Button";
 
 const ProfilePage = () => {
   const [coverImg, setCoverImg] = useState(null);
@@ -49,167 +52,160 @@ const ProfilePage = () => {
 
   return (
     <PageLayout>
-      <div className="flex-[4_4_0]  border-r border-gray-700 min-h-screen ">
+      <div className="profile-page">
         {/* HEADER */}
         {/* {isLoading && <ProfileHeaderSkeleton />} */}
         {!isLoading && !user && (
           <p className="text-center text-lg mt-4">User not found</p>
         )}
-        <div className="flex flex-col">
-          {!isLoading && user && (
-            <>
-              <div className="flex gap-10 px-4 py-2 items-center">
-                <Link to="/">
-                  <FaArrowLeft className="w-4 h-4" />
-                </Link>
-                <div className="flex flex-col">
-                  <p className="font-bold text-lg">{user?.fullName}</p>
-                  <span className="text-sm text-slate-500">
-                    {POSTS?.length} posts
-                  </span>
-                </div>
+
+        {!isLoading && user && (
+          <>
+            <div className="header">
+              <Link to="/">
+                <FaArrowLeft />
+              </Link>
+              <div className="header-info">
+                <p>{user?.fullName}</p>
+                <span>{POSTS?.length} posts</span>
               </div>
-              {/* COVER IMG */}
-              <div className="relative group/cover">
+            </div>
+            {/* COVER IMG */}
+            <div className="profile-img-sec">
+              <div className="cover-img-container">
                 <img
                   src={coverImg || user?.coverImg || "/cover.png"}
-                  className="h-52 w-full object-cover"
                   alt="cover image"
                 />
                 {isMyProfile && (
-                  <div
-                    className="absolute top-2 right-2 rounded-full p-2 bg-gray-800 bg-opacity-75 cursor-pointer opacity-0 group-hover/cover:opacity-100 transition duration-200"
-                    onClick={() => coverImgRef.current.click()}
-                  >
-                    <MdEdit className="w-5 h-5 text-white" />
+                  <div className="edit-cover-svg">
+                    <MdEdit onClick={() => coverImgRef.current.click()} />
                   </div>
                 )}
+              </div>
 
-                <input
-                  type="file"
-                  hidden
-                  ref={coverImgRef}
-                  onChange={(e) => handleImgChange(e, "coverImg")}
+              <input
+                type="file"
+                hidden
+                ref={coverImgRef}
+                onChange={(e) => handleImgChange(e, "coverImg")}
+              />
+              <input
+                type="file"
+                hidden
+                ref={profileImgRef}
+                onChange={(e) => handleImgChange(e, "profileImg")}
+              />
+              {/* USER AVATAR */}
+
+              <div className="profile-pic-container">
+                <Avatar
+                  src={
+                    profileImg || user?.profileImg || "/avatar-placeholder.png"
+                  }
+                  style={{ width: "100px", height: "100px" }}
                 />
-                <input
-                  type="file"
-                  hidden
-                  ref={profileImgRef}
-                  onChange={(e) => handleImgChange(e, "profileImg")}
-                />
-                {/* USER AVATAR */}
-                <div className="avatar absolute -bottom-16 left-4">
-                  <div className="w-32 rounded-full relative group/avatar">
-                    <img
-                      src={
-                        profileImg ||
-                        user?.profileImg ||
-                        "/avatar-placeholder.png"
-                      }
+                {isMyProfile && (
+                  <div className="edit-profile-svg">
+                    <MdEdit
+                      className="profile-edit"
+                      onClick={() => profileImgRef.current.click()}
                     />
-                    <div className="absolute top-5 right-3 p-1 bg-primary rounded-full group-hover/avatar:opacity-100 opacity-0 cursor-pointer">
-                      {isMyProfile && (
-                        <MdEdit
-                          className="w-4 h-4 text-white"
-                          onClick={() => profileImgRef.current.click()}
-                        />
-                      )}
-                    </div>
                   </div>
-                </div>
+                )}
               </div>
-              <div className="flex justify-end px-4 mt-5">
-                {/* {isMyProfile && <EditProfileModal />} */}
-                {!isMyProfile && (
-                  <button
-                    className="btn btn-outline rounded-full btn-sm"
-                    onClick={() => alert("Followed successfully")}
-                  >
-                    Follow
-                  </button>
-                )}
-                {(coverImg || profileImg) && (
-                  <button
-                    className="btn btn-primary rounded-full btn-sm text-white px-4 ml-2"
-                    onClick={() => alert("Profile updated successfully")}
-                  >
-                    Update
-                  </button>
-                )}
+            </div>
+            <div className="btn-sec">
+              {/* {isMyProfile && <EditProfileModal />} */}
+              {!isMyProfile && (
+                <Button
+                  onClick={() => alert("Followed successfully")}
+                  className="invert-btn"
+                >
+                  Follow
+                </Button>
+              )}
+              {(coverImg || profileImg) && (
+                <Button
+                  onClick={() => alert("Profile updated successfully")}
+                  className="invert-btn"
+                >
+                  Update
+                </Button>
+              )}
+            </div>
+
+            <div className="flex flex-col gap-4 mt-14 px-4">
+              <div className="flex flex-col">
+                <span className="font-bold text-lg">{user?.fullName}</span>
+                <span className="text-sm text-slate-500">
+                  @{user?.username}
+                </span>
+                <span className="text-sm my-1">{user?.bio}</span>
               </div>
 
-              <div className="flex flex-col gap-4 mt-14 px-4">
-                <div className="flex flex-col">
-                  <span className="font-bold text-lg">{user?.fullName}</span>
+              <div className="flex gap-2 flex-wrap">
+                {user?.link && (
+                  <div className="flex gap-1 items-center ">
+                    <>
+                      <FaLink className="w-3 h-3 text-slate-500" />
+                      <a
+                        href="https://youtube.com/@asaprogrammer_"
+                        target="_blank"
+                        rel="noreferrer"
+                        className="text-sm text-blue-500 hover:underline"
+                      >
+                        youtube.com/@asaprogrammer_
+                      </a>
+                    </>
+                  </div>
+                )}
+                <div className="flex gap-2 items-center">
+                  <IoCalendarOutline className="w-4 h-4 text-slate-500" />
                   <span className="text-sm text-slate-500">
-                    @{user?.username}
+                    Joined July 2021
                   </span>
-                  <span className="text-sm my-1">{user?.bio}</span>
-                </div>
-
-                <div className="flex gap-2 flex-wrap">
-                  {user?.link && (
-                    <div className="flex gap-1 items-center ">
-                      <>
-                        <FaLink className="w-3 h-3 text-slate-500" />
-                        <a
-                          href="https://youtube.com/@asaprogrammer_"
-                          target="_blank"
-                          rel="noreferrer"
-                          className="text-sm text-blue-500 hover:underline"
-                        >
-                          youtube.com/@asaprogrammer_
-                        </a>
-                      </>
-                    </div>
-                  )}
-                  <div className="flex gap-2 items-center">
-                    <IoCalendarOutline className="w-4 h-4 text-slate-500" />
-                    <span className="text-sm text-slate-500">
-                      Joined July 2021
-                    </span>
-                  </div>
-                </div>
-                <div className="flex gap-2">
-                  <div className="flex gap-1 items-center">
-                    <span className="font-bold text-xs">
-                      {user?.following.length}
-                    </span>
-                    <span className="text-slate-500 text-xs">Following</span>
-                  </div>
-                  <div className="flex gap-1 items-center">
-                    <span className="font-bold text-xs">
-                      {user?.followers.length}
-                    </span>
-                    <span className="text-slate-500 text-xs">Followers</span>
-                  </div>
                 </div>
               </div>
-              <div className="flex w-full border-b border-gray-700 mt-4">
-                <div
-                  className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 relative cursor-pointer"
-                  onClick={() => setFeedType("posts")}
-                >
-                  Posts
-                  {feedType === "posts" && (
-                    <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary" />
-                  )}
+              <div className="flex gap-2">
+                <div className="flex gap-1 items-center">
+                  <span className="font-bold text-xs">
+                    {user?.following.length}
+                  </span>
+                  <span className="text-slate-500 text-xs">Following</span>
                 </div>
-                <div
-                  className="flex justify-center flex-1 p-3 text-slate-500 hover:bg-secondary transition duration-300 relative cursor-pointer"
-                  onClick={() => setFeedType("likes")}
-                >
-                  Likes
-                  {feedType === "likes" && (
-                    <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary" />
-                  )}
+                <div className="flex gap-1 items-center">
+                  <span className="font-bold text-xs">
+                    {user?.followers.length}
+                  </span>
+                  <span className="text-slate-500 text-xs">Followers</span>
                 </div>
               </div>
-            </>
-          )}
+            </div>
+            <div className="flex w-full border-b border-gray-700 mt-4">
+              <div
+                className="flex justify-center flex-1 p-3 hover:bg-secondary transition duration-300 relative cursor-pointer"
+                onClick={() => setFeedType("posts")}
+              >
+                Posts
+                {feedType === "posts" && (
+                  <div className="absolute bottom-0 w-10 h-1 rounded-full bg-primary" />
+                )}
+              </div>
+              <div
+                className="flex justify-center flex-1 p-3 text-slate-500 hover:bg-secondary transition duration-300 relative cursor-pointer"
+                onClick={() => setFeedType("likes")}
+              >
+                Likes
+                {feedType === "likes" && (
+                  <div className="absolute bottom-0 w-10  h-1 rounded-full bg-primary" />
+                )}
+              </div>
+            </div>
+          </>
+        )}
 
-          <Posts />
-        </div>
+        <Posts />
       </div>
     </PageLayout>
   );
