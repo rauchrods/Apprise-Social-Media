0234285c28c9c942c5c590a359402f8020ad4087
rauchrods <rauchrodrigues@gmail.com>
2025-06-22 21:30:25 +0530

    added rate limit time to 15 minutes

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 // Rate limiting configuration
 export const limiter = rateLimit({
-  windowMs: 5 * 60 * 1000, // 5 minutes
+  windowMs: 15 * 60 * 1000, // 15 minutes
   max: 100, // Limit each IP to 100 requests per windowMs
   message: {
     success: false,
