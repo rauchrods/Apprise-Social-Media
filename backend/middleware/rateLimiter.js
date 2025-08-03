88abe3cfa268d59b482b7f01b3c65609538617fe
rauchrods <rauchrodrigues@gmail.com>
2025-08-03 13:05:25 +0530

    refactored ramtelimiting from 15mins to 1min

--- backend/middleware/rateLimiter.js
+++ backend/middleware/rateLimiter.js
@@ -2,7 +2,7 @@ import rateLimit from "express-rate-limit";
 
 // Rate limiting configuration
 export const limiter = rateLimit({
-  windowMs: 15 * 60 * 1000, // 15 minutes
+  windowMs: 1 * 60 * 1000, // 1 minute
   max: 100, // Limit each IP to 100 requests per windowMs
   message: {
     success: false,
